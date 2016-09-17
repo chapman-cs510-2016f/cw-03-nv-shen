367049948e3daa2220d6fdcfea4d27b2f4a1d51e
helen77 <zny_ok1989@hotmail.com>
2016-09-17 18:22:58 +0000

    Changed fib.py

--- fib.py
+++ fib.py
@@ -7,22 +7,29 @@ def test_main():
 	"""Test main function from sequences 1 exectued successfully"""
 	arg = argv
 	computed = main(arg)
-	expected = 0
+	expected = None
 	success = computed == expected
 	message = 'Computed %s, expected %s' % (computed, expected)
 	assert success, message	
+
+def main(argv):
+	pass
 
 
 if __name__ == "__main__":	
 	"""Call the fibonacci function from sequesces1 moduler"""
 	try:
 		n = argv[1]
-		print fibonacci(n)
+		fibList=fibonacci(n)
 	except IndexError:
 		print 'No Command-line argument was inputed.'
-
+		exit(1)
+	if isinstance(fibList, list):
+		print fibList[-1]
+	else:
+		print fibList
 	
-	"""Call main function from sequesces1 moduler"""
+	
 	main(argv)
 
 	test_main()
